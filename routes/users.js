b910e59740d865aa37a31e5259da0c3a2cbec6ab
pwnzr <pvvner@yandex.ru>
2017-04-23 13:53:16 +0300

    Protecting routes

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,14 @@
 var express = require('express');
 var router = express.Router();
 
+// MW
+router.use('/', function(req, res, next) {
+  if (!req.user) {
+    res.redirect('/');
+  }
+  next();
+});
+
 /* GET users listing. */
 router.get('/', function (req, res, next) {
   // user is added to req by Passport
